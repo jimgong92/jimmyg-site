08935d668446a4394fbb64100e0d887e74f5e205
Jimmy Gong <gong.jim@gmail.com>
2016-02-01 19:14:41 -0800

    Adds base styling

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,16 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import App from './containers/App';
+import '../styles/css/main.css';
 
 const store = configureStore();
+
+// Enable hot reloading of styles
+if(module.hot) {
+  module.hot.accept('../styles/css/main.css', function() {
+    require('../styles/css/main.css');
+  });
+}
 
 render(
   <Provider store={store}>
