f8fca503aa7d4f26be399fdf3240945efff76ff7
Jimmy Gong <gong.jim@gmail.com>
2016-02-16 16:20:30 -0800

    Touches up styling

--- src/js/components/ConnectModal.js
+++ src/js/components/ConnectModal.js
@@ -9,7 +9,8 @@ class ConnectModal extends Component {
     const { closeModal, isOpen } = this.props;
     const className = cx({
       'modal': true,
-      'modal-close': !isOpen
+      'modal-close': !isOpen,
+      'connect-modal': true
     });
     return (
       <div className={ className }>
@@ -17,30 +18,32 @@ class ConnectModal extends Component {
           Connect
         </span>
         <div className="modal-content center">
-          <div className="icon-container inactive-icon github">
-            <a target="_blank" href="https://github.com/jimgong92">
-              <i className="fa fa-github-square fa-5x icon" />
-            </a>
-          </div>
-          <div className="icon-container inactive-icon facebook">
-            <a target="_blank" href="https://www.facebook.com/jimmy.gong.39">
-              <i className="fa fa-facebook-square fa-5x icon" />
-            </a>
-          </div>
-          <div className="icon-container inactive-icon linkedin">
-            <a target="_blank" href="https://www.linkedin.com/in/jimgong92">
-              <i className="fa fa-linkedin-square fa-5x icon" />
-            </a>
-          </div>
-          <div className="icon-container active-icon gmail">
-            <a target="_blank" href="mailto:gong.jim@gmail.com">
-              <i className="fa fa-envelope-square fa-5x icon" />
-            </a>
-          </div>
-          <div className="icon-container inactive-icon twitter">
-            <a target="_blank" href="https://twitter.com/jimmyg___">
-              <i className="fa fa-twitter-square fa-5x icon" />
-            </a>
+          <div className="connect-menu">
+            <div className="icon-container inactive-icon github">
+              <a target="_blank" href="https://github.com/jimgong92">
+                <i className="fa fa-github-square fa-5x icon" />
+              </a>
+            </div>
+            <div className="icon-container inactive-icon facebook">
+              <a target="_blank" href="https://www.facebook.com/jimmy.gong.39">
+                <i className="fa fa-facebook-square fa-5x icon" />
+              </a>
+            </div>
+            <div className="icon-container inactive-icon linkedin">
+              <a target="_blank" href="https://www.linkedin.com/in/jimgong92">
+                <i className="fa fa-linkedin-square fa-5x icon" />
+              </a>
+            </div>
+            <div className="icon-container active-icon gmail">
+              <a target="_blank" href="mailto:gong.jim@gmail.com">
+                <i className="fa fa-envelope-square fa-5x icon" />
+              </a>
+            </div>
+            <div className="icon-container inactive-icon twitter">
+              <a target="_blank" href="https://twitter.com/jimmyg___">
+                <i className="fa fa-twitter-square fa-5x icon" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
